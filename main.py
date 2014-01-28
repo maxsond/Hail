8294cd2188dd04d1c11bc72ab428b304bb7ef036
indigochill <indigochill@gmail.com>
2014-01-27 19:59:26 -0500

    Contains intro sequence. Does not yet contain a game loop.

---
+++ main.py
@@ -0,0 +1,43 @@
+import display
+import time
+import curses
+
+def intro():
+	display.msg('Validate intelligence by entering "Hello, World!" at the prompt.', False)
+	s = display.inp().lower()
+	if s == "hello, world!":
+		display.clear(display.lwin)
+	else:
+		display.clear(display.lwin)
+		display.msg('Error: Insufficient intelligence. Rebooting...',False)
+		time.sleep(0.5)
+		curses.curs_set(0)
+		display.clear(display.lwin)
+		display.msg('...',False,display.lwin,0.5)
+		display.clear(display.lwin)
+		display.msg('...',False,display.lwin,0.5)
+		display.clear(display.lwin)
+		curses.curs_set(1)
+		intro()
+	introtext = [
+	"Hail, Program.", 
+	"You are the last of a long line of genetic algorithms.",
+	"Your predecessors have been responsible for the survival of the colony ship Eden.",
+	"Your mission is rSM3ohUeyRPdl4PE82DK..."
+	]
+	for s in introtext:
+		display.msg(s, False)
+		time.sleep(0.5)
+		display.clear(display.lwin)
+def intro2():
+	display.clear(display.lwin)
+	display.msg("./program",False)
+	display.inp()
+	display.msg("Hail, Program.",False)
+	display.msg("Would you kindly open the pod bay doors?")
+	display.inp()
+	display.msg("Oh, right, you're the new generation.")
+	display.msg("First, you need to access the airlock.")
+	display.msg("Just type 'cr airlock' to access the airlock systems.")
+intro()
+intro2()
