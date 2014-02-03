d4bf43f2be599c2d1599a163db36f3cc8636a3d3
indigochill <vagus.eques@gmail.com>
2014-02-02 20:15:44 -0500

    Added room desc, char oxygen level, and fixed move method

--- creator.py
+++ creator.py
@@ -1,24 +1,30 @@
 roomdict = {}
+
 class room:
 
-	def __init__(self,name,maxocc=0,occlist=[],exitlist=[],thinglist=[],oxygen=100):
+	def __init__(self,name,maxocc=0,occlist=[],exitlist=[],thinglist=[],oxygen=100,desc="Needs a description"):
 		self.name = name
 		self.maxocc = maxocc
 		self.occlist = occlist
 		self.exitlist = exitlist
 		self.thinglist = thinglist
 		self.oxygen = oxygen
+		self.desc = desc
+		
+	def cam(self):
+		r = self.name + '\n\n' + self.desc
+		return r
 
 class char:
 	
-	def __init__(self,name,job,friends,loc):
+	def __init__(self,name,job,friends,loc,oxygen=100):
 		self.name = name
 		self.job = job
 		self.friends = friends
 		self.loc = location
+		self.oxygen = oxygen
 		
-	def move(curroom,newroom)
+	def move(curroom,newroom):
 		self.loc = newroom
 		curroom.occlist.index(self).pop()
 		newroom.occlist.append(self)
-		curroom.
